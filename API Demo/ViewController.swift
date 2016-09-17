75024917f551db24eeea8d230f9aac467dad81f9
Lamar Greene <github@lamargreene.com>
2016-09-17 13:26:56 -0500

    Storyboard added
    The elements to the interface have been added and linked, and the code
    to check for spaces in city names.

--- API Demo/ViewController.swift
+++ API Demo/ViewController.swift
@@ -8,29 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var forecastLabel: UILabel!
+    
+    @IBAction func submit(_ sender: UIButton) {
+        
+        if let cityName = cityTextField.text?.replacingOccurrences(of: " ", with: "-") {
+            
+            getForecast(cityName: cityName)
+            
+        }
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.cityTextField.delegate = self
+    }
+    
+    
+    func getForecast(cityName: String) {
         
-        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=\(API_Key)")!
+        
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(API_Key)")!
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             
             if error != nil {
-            
+                
                 print(error)
-            
+                
             } else {
-            
+                
                 if let urlContent = data {
                     
                     do {
-                    
-                    let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-                
-                    print(jsonResult)
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                        
+                        print(jsonResult)
                         
                         // to get name element only
                         print(jsonResult["name"])
@@ -39,13 +61,20 @@ class ViewController: UIViewController {
                             
                             print(description)
                             
+                            DispatchQueue.main.sync(execute: {
+                                
+                                self.forecastLabel.text = description
+                                
+                            })
+                            
+                            
                         }
                         
                         
                     } catch {
-                    
-                        print("JSON processing failed")
-                    
+                        
+                        print(">>>>JSON processing failed")
+                        
                     }
                     
                 }
@@ -55,14 +84,35 @@ class ViewController: UIViewController {
         }
         
         task.resume()
+
         
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+//    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//        self.view.endEditing(true)
+//    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        cityTextField.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+    // Add to viewDidLoad -->> self.textField.delegate = self
+    // Add UITextFieldDelegate to ViewController
 
 }
 
