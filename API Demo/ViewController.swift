b845e753fd4881b329b23c2596d6e5992166fc37
Lamar Greene <github@lamargreene.com>
2016-09-17 10:20:52 -0500

    API working
    The API link has been inserted and is pulling the JSON. The JSON has
    been parsed and needed info extracted

--- API Demo/ViewController.swift
+++ API Demo/ViewController.swift
@@ -14,7 +14,48 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print(API_Key)
+        let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=Denver&appid=\(API_Key)")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+            
+                print(error)
+            
+            } else {
+            
+                if let urlContent = data {
+                    
+                    do {
+                    
+                    let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
+                
+                    print(jsonResult)
+                        
+                        // to get name element only
+                        print(jsonResult["name"])
+                        
+                        if let description = (((jsonResult as? NSDictionary)?["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
+                            
+                            print(description)
+                            
+                        }
+                        
+                        
+                    } catch {
+                    
+                        print("JSON processing failed")
+                    
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
